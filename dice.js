a8263964f4dd8f1ac7015faec5060fe077adb470
Terrys101 <97230300+Terrys101@users.noreply.github.com>
2022-02-09 09:07:04 +0000

    update

--- dice.js
+++ dice.js
@@ -1,10 +1,23 @@
-const firstRandom = Math.floor(Math.random()
-*6) +1
 
+const firstRandomNum = Math.floor(Math.random()
+*6)+1
 
-
-const firstDiceImage = 'images/dice' + firstRandom + 'png';
+const firstDiceImage = 'images/dice' + firstRandomNum + '.png';
 
 document.querySelectorAll('img')[0].setAttribute('src',firstDiceImage)
 
 
+const secondRandomNum = Math.floor(Math.random()
+*6)+1
+
+const secondDiceImage = 'images/dice' + secondRandomNum + '.png';
+
+document.querySelectorAll('img')[1].setAttribute('src',secondDiceImage)
+
+if (firstRandomNum > secondRandomNum){
+    document.querySelector('h1').innerHTML = 'The Winner is User1';  
+} else if  ( firstRandomNum < secondRandomNum ){
+document.querySelector('h1').innerHTML = 'The Winner is User2';
+} else{ 
+document.querySelector('h1').linnerHTML = 'Its a Draw!';
+}
